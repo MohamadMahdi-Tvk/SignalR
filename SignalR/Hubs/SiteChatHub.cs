feef1c223ccd16bc9688bdc30a02baa0bb9f27a3
mohamadmahditavakoli <mohamadmahditavakoli8@gmail.com>
2024-08-02 23:35:02 +0330

    Add project files.

---
+++ SignalR/Hubs/SiteChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs;
+
+public class SiteChatHub : Hub
+{
+    public async Task SendNewMessage(string Sender, string Message)
+    {
+        await Clients.All.SendAsync("getNewMessage", Sender, Message, DateTime.Now.ToShortDateString());
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        return base.OnDisconnectedAsync(exception);
+    }
+}
---
+++ SignalR/Program.cs
@@ -0,0 +1,40 @@
+using SignalR.Hubs;
+
+namespace SignalR
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddControllersWithViews();
+
+
+            builder.Services.AddSignalR();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapHub<SiteChatHub>("/chathub");
+            app.Run();
+        }
+    }
+}
