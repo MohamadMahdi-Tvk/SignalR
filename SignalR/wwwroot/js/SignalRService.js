e13cb7a3b42b4a38c01dc64fc1962580de7f074c
mohamadmahditavakoli <mohamadmahditavakoli8@gmail.com>
2024-08-04 21:00:30 +0330

    Feat: Sending Messages From Client To Server

--- SignalR/wwwroot/js/SignalRService.js
+++ SignalR/wwwroot/js/SignalRService.js
@@ -11,12 +11,34 @@ connection.start();
 
 //نمایش چت باکس برای کاربر
 function showChatDialog() {
+
     chatBox.css("display", "block");
 }
 
 function Init() {
+
     setTimeout(showChatDialog, 2000);
+
+    var NewMessageForm = $("#NewMessageForm");
+
+    NewMessageForm.on("submit", function (e) {
+
+        e.preventDefault();
+
+        var message = e.target[0].value;
+
+        e.target[0].value = '';
+
+        sendMessage(message);
+    });
 }
+
+function sendMessage(text) {
+
+    connection.invoke('SendNewMessage', "بازدید کننده", text);
+}
+
 $(document).ready(function () {
+
     Init();
 });
